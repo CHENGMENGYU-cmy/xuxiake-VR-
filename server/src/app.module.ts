9d7337641f548b46eeead8ee9a7cd6a7e0866982
“itc <hello@itcast.cn>
2026-07-18 18:02:11 +0800

    auto: update app.module.ts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -48,7 +48,7 @@ import { JwtModule } from '@nestjs/jwt';
       ConversationParticipant, UserFollow,
       InterestTag, UserInterest, Community, CommunityTag,
       CommunityAnnouncement, CommunityRole, CommunityChallenge, CommunityChallengeEntry,
-      MessageReaction,
+      MessageReaction, LocationShare,
     ]),
     JwtModule.registerAsync({
       imports: [ConfigModule],
