ebda39d4112cb2a0b9380c0fcf90efa0c9e4ada8
“itc <hello@itcast.cn>
2026-07-27 20:59:47 +0800

    auto: update migrate-ar-sessions.sql

--- server/sql/migrate-disappearing-messages.sql
+++
@@ -1,3 +0,0 @@
--- 消失消息字段
-ALTER TABLE `conversations` ADD COLUMN `is_disappearing` tinyint(1) NOT NULL DEFAULT 0 AFTER `updated_at`;
-ALTER TABLE `conversations` ADD COLUMN `disappear_seconds` int NOT NULL DEFAULT 0 AFTER `is_disappearing`;
