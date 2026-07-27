4782c5c0f354838e410c58b12bb8aa3e92152662
“itc <hello@itcast.cn>
2026-07-27 11:10:54 +0800

    auto: update next.config.ts

--- web/next.config.ts
+++ web/next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   devIndicators: false,
   images: {
     remotePatterns: [
+      { protocol: 'http', hostname: 'localhost' },
       { protocol: 'https', hostname: 'picsum.photos' },
       { protocol: 'https', hostname: 'api.dicebear.com' },
     ],
