4297dba3f8727ddfcf0501b05e12c515eaceb8e1
“itc <hello@itcast.cn>
2026-07-28 20:44:23 +0800

    auto: update posts.controller.ts

--- server/src/modules/posts/posts.controller.ts
+++ server/src/modules/posts/posts.controller.ts
@@ -19,6 +19,7 @@ export class PostsController {
     private readonly postsService: PostsService,
     private readonly reviewService: ReviewService,
     private readonly authService: AuthService,
+    private readonly aiService: AiService,
     @InjectRepository(User) private readonly userRepo: Repository<User>,
     @InjectRepository(VideoComment) private readonly vcRepo: Repository<VideoComment>,
     @InjectRepository(AudioPlaylist) private readonly playlistRepo: Repository<AudioPlaylist>,
