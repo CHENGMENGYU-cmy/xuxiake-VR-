7910b900b77371bec4517a4dcac2f937c0bb2ca6
“itc <hello@itcast.cn>
2026-07-28 20:28:45 +0800

    auto: update app.module.ts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -31,6 +31,7 @@ import { CommunityChallenge } from './entities/community-challenge.entity.js';
 import { CommunityChallengeEntry } from './entities/community-challenge-entry.entity.js';
 import { MessageReaction } from './entities/message-reaction.entity.js';
 import { LocationShare } from './entities/location-share.entity.js';
+import { RecommendationFeedback } from './entities/recommendation-feedback.entity.js';
 import { getDatabaseConfig } from './config/database.config.js';
 import { JwtModule } from '@nestjs/jwt';
 
