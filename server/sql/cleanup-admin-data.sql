e1f1c667a173ba8365a7e6e35c645a14814378a2
“itc <hello@itcast.cn>
2026-07-30 20:01:26 +0800

    auto: update cleanup-admin-data.sql

---
+++ server/sql/cleanup-admin-data.sql
@@ -0,0 +1,19 @@
+-- 清理xuxiake(u1)管理员账号的所有用户内容
+-- u1只做管理，不发布内容
+
+DELETE FROM posts WHERE author_id='u1';
+DELETE FROM comments WHERE user_id='u1';
+DELETE FROM likes WHERE user_id='u1';
+DELETE FROM collection_posts WHERE collection_id IN (SELECT id FROM collections WHERE creator_id='u1');
+DELETE FROM collections WHERE creator_id='u1';
+DELETE FROM user_follows WHERE follower_id='u1' OR following_id='u1';
+DELETE FROM notifications WHERE user_id='u1';
+DELETE FROM messages WHERE sender_id='u1';
+DELETE FROM conversation_participants WHERE user_id='u1';
+DELETE FROM community_roles WHERE user_id='u1';
+DELETE FROM user_interests WHERE user_id='u1';
+DELETE FROM reports WHERE reporter_id='u1';
+DELETE FROM journeys WHERE user_id='u1';
+DELETE FROM recommendation_feedback WHERE user_id='u1';
+
+SELECT 'xuxiake cleanup done' AS result;
