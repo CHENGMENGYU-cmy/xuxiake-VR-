873c1d1c28d0cb8a045507a9e7a791e5bf806296
“itc <hello@itcast.cn>
2026-07-31 17:03:24 +0800

    auto: update page.tsx

--- web/src/app/(main)/diaries/page.tsx
+++ web/src/app/(main)/diaries/page.tsx
@@ -417,10 +417,10 @@ function DiariesContent() {
                           </div>
 
                           {/* 右侧大图（如果有） */}
-                          {post.mediaItems.length > 0 && post.mediaItems[0].type === 'IMAGE' && post.mediaItems[0].thumbnailUrl && (
+                          {post.mediaItems.length > 0 && post.mediaItems[0].type === 'IMAGE' && (post.mediaItems[0].thumbnailUrl || post.mediaItems[0].url) && (
                             <div className="shrink-0 w-24 h-24 rounded-md overflow-hidden bg-muted">
                               <img
-                                src={post.mediaItems[0].thumbnailUrl}
+                                src={post.mediaItems[0].thumbnailUrl || post.mediaItems[0].url}
                                 alt=""
                                 className="w-full h-full object-cover"
                               />
