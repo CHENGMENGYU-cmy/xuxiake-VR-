8008f946f9066c74c1c64a38f44b63a3d256daca
“itc <hello@itcast.cn>
2026-08-04 18:32:59 +0800

    auto: update sidebar.tsx

--- web/src/components/layout/sidebar.tsx
+++ web/src/components/layout/sidebar.tsx
@@ -19,6 +19,7 @@ import {
   Users,
   TrendingUp,
   Camera,
+  LayoutGrid,
 } from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
