fbb2119c0547a9e759a1ad34e78e3a220663c50c
“itc <hello@itcast.cn>
2026-08-04 22:05:58 +0800

    auto: update navbar.tsx

--- web/src/components/layout/navbar.tsx
+++ web/src/components/layout/navbar.tsx
@@ -88,7 +88,7 @@ export function Navbar() {
     };
     socket.on('notification:new', handleNewNotification);
     return () => { socket.off('notification:new', handleNewNotification); };
-  }, [user, isAuthenticated, setNotifUnreadCount]);
+  }, [user, isAuthenticated, authReady, setNotifUnreadCount]);
 
   const handleSearch = (e: React.FormEvent) => {
     e.preventDefault();
