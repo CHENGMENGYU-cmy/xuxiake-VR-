7e59b26c6cbd1156fc04dcee13d4bfdadb467f02
“itc <hello@itcast.cn>
2026-08-04 19:22:47 +0800

    auto: update sync.module.ts

--- server/src/modules/sync/sync.module.ts
+++ server/src/modules/sync/sync.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtModule } from '@nestjs/jwt';
 import { SyncController } from './sync.controller.js';
 import { SyncService } from './sync.service.js';
 import { Post } from '../../entities/post.entity.js';
 import { MediaItem } from '../../entities/media-item.entity.js';
+import { AuthModule } from '../auth/auth.module.js';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Post, MediaItem]),
-    JwtModule,
+    AuthModule,
   ],
   controllers: [SyncController],
   providers: [SyncService],
