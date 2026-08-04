f30dec9df97444c4ac0098dec1b53fde1a8472da
“itc <hello@itcast.cn>
2026-08-04 17:55:59 +0800

    auto: update ai.service.ts

--- server/src/modules/posts/ai.service.ts
+++ server/src/modules/posts/ai.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, In } from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
