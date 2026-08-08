17dbe035a985b760bbf12e0251180f978f645172
“itc <hello@itcast.cn>
2026-08-08 15:24:36 +0800

    auto: update posts.service.ts

--- server/src/modules/posts/posts.service.ts
+++ server/src/modules/posts/posts.service.ts
@@ -1306,7 +1306,7 @@ export class PostsService {
     const collection = await this.collectionRepo.findOne({ where: { id: collectionId } });
     if (!collection) throw new NotFoundException('合集不存在');
     if (collection.creatorId !== userId) throw new NotFoundException('无权操作此合集');
-    const post = await this.postRepo.findOne({ where: { id: postId } });
+    const post = await this.postRepo.findOne({ where: { id: postId, deletedAt: IsNull() } });
     if (!post) throw new NotFoundException('内容不存在');
 
     const existing = await this.collectionPostRepo.findOne({ where: { collectionId, postId } });
