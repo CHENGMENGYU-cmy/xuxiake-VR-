def74faaa0f92359b5395808230b0fb7b9f01691
“itc <hello@itcast.cn>
2026-08-08 15:37:30 +0800

    auto: update users.controller.ts

--- server/src/modules/users/users.controller.ts
+++ server/src/modules/users/users.controller.ts
@@ -425,7 +425,7 @@ export class UsersController {
 
     console.log('[DEBUG getUserPosts] SQL=', qb.getSql());
     const posts = await qb.getMany();
-    console.log('[DEBUG getUserPosts] 列表条数=', posts.length, 'total(user.id)请求参数=', (qb.getQueryAndParameters())[1]?.userId);
+    console.log('[DEBUG getUserPosts] 列表条数=', posts.length);
     const hasMore = posts.length > take;
     const data = posts.slice(0, take);
 
