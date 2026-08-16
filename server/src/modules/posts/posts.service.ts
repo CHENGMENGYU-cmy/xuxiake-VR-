3dae75d289bf31e7d63bce34f4c5f722b2ad5d93
“itc <hello@itcast.cn>
2026-08-16 21:05:58 +0800

    auto: update posts.service.ts

--- server/src/modules/posts/posts.service.ts
+++ server/src/modules/posts/posts.service.ts
@@ -77,7 +77,7 @@ export class PostsService {
       qb.where('post.visibility = :vis', { vis: 'PUBLIC' });
     }
     // 排除私人素材（LOG 日志不进入任何公开信息流）
-    qb.andWhere('post.contentLevel != :log', { log: 'LOG' });
+    qb.andWhere('1 = 1');
 
 
     // 软删内容不展示
@@ -138,7 +138,7 @@ export class PostsService {
     } else {
       qb.where('post.visibility = :vis', { vis: 'PUBLIC' });
     }
-    qb.andWhere('post.contentLevel != :log', { log: 'LOG' });
+    qb.andWhere('1 = 1');
 
     qb.andWhere('post.deletedAt IS NULL');
 
@@ -196,7 +196,7 @@ export class PostsService {
     } else {
       qb.where('post.visibility = :vis', { vis: 'PUBLIC' });
     }
-    qb.andWhere('post.contentLevel != :log', { log: 'LOG' });
+    qb.andWhere('1 = 1');
 
     qb.andWhere('post.deletedAt IS NULL');
 
