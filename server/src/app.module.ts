b87c9585161c401f4cd393179f57bda6ca5aba7f
“itc <hello@itcast.cn>
2026-08-23 16:16:19 +0800

    auto: update app.module.ts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -34,6 +34,7 @@ import { CommunityChallengeEntry } from './entities/community-challenge-entry.en
 import { MessageReaction } from './entities/message-reaction.entity.js';
 import { LocationShare } from './entities/location-share.entity.js';
 import { RecommendationFeedback } from './entities/recommendation-feedback.entity.js';
+import { UserCommunityInteraction } from './entities/user-community-interaction.entity.js';
 import { VideoComment } from './entities/video-comment.entity.js';
 import { AudioPlaylist } from './entities/audio-playlist.entity.js';
 import { getDatabaseConfig } from './config/database.config.js';
